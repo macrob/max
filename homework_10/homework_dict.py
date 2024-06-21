c2f04239a2f1e89ff783e2668c84a32e68c02bb2
Macrob <se@xambo.io>
2024-06-21 09:58:47 +0200

    up

--- homework_10/homework_dict.py
+++ homework_10/homework_dict.py
@@ -4,12 +4,66 @@
 student[outer_dict_key][inner_dict_key]
 
 Є дані студентів (комбінація імені та прізвища унікальна), що зберігаються за допомогою словника
-1 - програмно добавити одного студента, з заповненням усіх полів (вік - від 18 до 40, цілочисельне значення, 
-    бал від 0 до 100 (інт чи флоат)
-2 - створити і вивести на екран список студентів (імя та прізвище та середній бал), у яких середній бал більше 90
-    сам формат наповнення цього списку up to you
-3 - визначити середній бал по групі
-4 - при відсутності номеру телефону у студента записати номер батьків (номер на ваш вибір)
+1 - 
+1.1 програмно добавити одного студента, 
+1.1.1 Зробити функцію яка виведе в браузер форму та повертає дані студента:
+1.1.1.1 Які данні і скільки полів повина мати форма, дивимось на дані в БД (students)
+
+1.1.2 зробити функцію яка буде добавляти студента, аргумент функціі данні студента з форми, функція нічого не повертає, але добавляє в БД
+1.1.2.1 зробити функцію яка буде робити конвертацію/переклад англіских назв ключей на україньску. Тобто аргумент функції англіське слово, 
+повертає фунція україньске слово.
+
+якщо подивись на формат данних у бд та данні які повернулись з форми, то вони мають разні ключи, в бд в нас
+ключами є 
+Пошта
+Вік
+Номер телефону
+Середній бал
+з форми input_student поверає данні с ключами на англіскій мові. 
+
+1.1.2.2 зробити функцію яка повертає ключ для БД (students - наша БД), наприклад 
+input_student = {
+    first_name: 'Сергій'
+    last_name: 'Єпішкін'
+    age: 37
+    .... інші поля
+}
+Ключем до БД є 'Сергій Єпішкін'
+
+
+2 -
+2.1 створити список студентів (імя та прізвище та середній бал), у яких середній бал більше 90
+2.1.1 функція яка повертає список гарних студентів, аргументи функціі, дикшенарі studentds, там середній бал по якому ми робимо фільтрацію
+функція повертає список в форматі [ФІО, середній бал]
+
+2.2 вивести на екран, формат наповнення цього списку up to you
+2.2.1 створити функцію яка виводить на єкран список гарніх студентів. аргумент функціі
+список в форматі good_students = [
+    ['Сергій Єпішкін', 80],
+    ['Максим Єпішкін', 90],
+]. Виводим інво таблицєю 
+
+3
+3.1 - визначити середній бал по групі
+3.1.1 функція аргумент якої studentds БД, функція повертає середній бал
+3.2 - вивести на екран
+3.2.1 функція виводу на єкран середнього балу, аргумент функції серелній бал, нічого не повертає
+
+4 вывести на экран список студентів, при відсутності номеру телефону у студента записати номер батьків (номер на ваш вибір)
+4.1 зробити список
+4.1.1 функція яка повертає список студентів, аргумент функціі students (наша БД), функця повертає
+список [
+    [
+        name,
+        avg_mark,
+        phone, - якщо телефон не вказаний ставимо якись дефолтний, чи повідомлення що номер не вказано
+        email - якщо імейл не вказаний ставимо якись дефолтний, чи повідомлення що імейл не вказано
+    ]
+]
+4.2 вывести на экран список студентів
+4.2.1
+зробити функцію яка виводить через таблицю список стундентів. аргумент функції 
+cписок яки повернулся функція 4.1.1
 
 не забувайте виводити інформаційні повідомлення щодо інформації, яку ви виводите
 """
@@ -53,20 +107,27 @@ from utils import reload_page
 def translate_field(key):
     return constants.FORM_KEYS_TRANSLATE[key]
 
-def get_student_key(input_student: dict) -> str:
+EMAIL = translate_field('email')
+AGE = translate_field('age')
+PHONE = translate_field('phone')
+AVG_MARK = translate_field('avg_mark')
+
+
+
+def get_dict_key(input_student: dict) -> str:
     fname = input_student.get('fname')
     lname = input_student.get('lname')
     return fname +'' + lname
     
 def add_student(input_student):
     student = {
-        translate_field('email'): input_student.get('email'),
-        translate_field('age'): input_student.get('age'),
-        translate_field('phone'): input_student.get('phone'),
-        translate_field('avg_mark'): input_student.get('avg_mark'),
+        EMAIL: input_student.get('email'),
+        AGE: input_student.get('age'),
+        PHONE: input_student.get('phone'),
+        AVG_MARK: input_student.get('avg_mark'),
     }
     
-    key = get_student_key(input_student)
+    key = get_dict_key(input_student)
     students[key] = student
     pass
 
@@ -94,8 +155,9 @@ def request_student_data():
 def get_good_students(students: dict, avg_mark: int) -> list:
     result = []
     for name, studentData in students.items():
-        if studentData[translate_field('avg_mark')] >= avg_mark:
-            result.append([name, studentData[translate_field('avg_mark')]])
+        student_avg_mark = studentData[AVG_MARK];
+        if student_avg_mark >= avg_mark:
+            result.append([name, student_avg_mark])
     return result
 
 
@@ -109,7 +171,8 @@ def show_good_students(good_students: list):
 def get_avg_group_mark(students: dict) -> float:
     result = 0
     for student in students.values():
-        result += student[translate_field('avg_mark')]
+        student_avg_mark = student[AVG_MARK]
+        result += student_avg_mark
     return result / len(students)
 
 
@@ -122,9 +185,9 @@ def get_students(students: dict) -> list:
     for name, studentData in students.items():
         result.append([
             name, 
-            studentData.get(translate_field('avg_mark')),
-            studentData.get(translate_field('phone')) or constants.MSG_PHONE_NOT_FOUND,
-            studentData.get(translate_field('email')) or constants.MSG_EMAIL_NOT_FOUND,
+            studentData.get(AVG_MARK),
+            studentData.get(PHONE) or constants.MSG_PHONE_NOT_FOUND,
+            studentData.get(EMAIL) or constants.MSG_EMAIL_NOT_FOUND,
         ])
 
     return result
@@ -156,3 +219,18 @@ def main():
 
 if __name__ == '__main__':
     start_server(main, port=config.SERVER_PORT, debug=config.SERVER_DEBUG)
+
+
+def get_avg_mark_group(students: dict) -> float:
+    result = 0
+
+    students_list = students.values()
+    students_count = len(students_list)
+
+    for student_data in students_list:
+        student_avg_mark = student_data.get(translate_key('avg_mark'))
+        result += student_avg_mark
+
+    result /= students_count
+
+    return result
